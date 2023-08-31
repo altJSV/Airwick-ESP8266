d7196df3eebae52697a5accc0023d157ffffb868
altJSV <alienlair@yandex.ru>
2023-08-31 20:43:26 +0300

    Небольшие правки параметров SSDP

--- SSDP.ino
+++ SSDP.ino
@@ -13,14 +13,13 @@ void SSDP_init(void) {
   saveConfig();                 // Функция сохранения данных во Flash
   HTTP.send(200, "text/plain", "OK"); // отправляем ответ о выполнении
   });
-  //Если версия  2.0.0 закаментируйте следующую строчку
   SSDP.setDeviceType("upnp:rootdevice");
   SSDP.setSchemaURL("description.xml");
   SSDP.setHTTPPort(80);
   SSDP.setName(jsonRead(configSetup, "SSDP"));
   SSDP.setSerialNumber(chipID);
   SSDP.setURL("/");
-  SSDP.setModelName("AirWick");
+  SSDP.setModelName("AirWick ESP8266");
   SSDP.setModelNumber(jsonRead(configSetup, "SSDP") + FLL_VERSION);
   
   
